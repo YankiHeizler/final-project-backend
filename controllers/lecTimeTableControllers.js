9679d810d763f58f0bb6b6f479c06768cf4f6a01
EliezerKudrin <elik318@gmail.com>
2024-06-25 12:41:56 +0300

    ok

--- controllers/lecTimeTableControllers.js
+++ controllers/lecTimeTableControllers.js
@@ -20,17 +20,47 @@ exports.getLecTimeTable = asyncHandler(async (req, res) => {
 
     const dates = []
 
+
+    function subtractDays(date, days) {
+        const millisecondsPerDay = 1000 * 60 * 60 * 24;
+        return new Date(date.getTime() - days * millisecondsPerDay);
+    }
+
+    function plusDay(date, days) {
+        const millisecondsPerDay = 1000 * 60 * 60 * 24;
+        return new Date(date.getTime() + days * millisecondsPerDay);
+    }
+      
     let curr = new Date(UserFirstDate); // get current date
-    let first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
-    dates.push(new Date(curr.setDate(first)).toLocaleDateString('en-GB').replaceAll('/', '.'))
+    let firstDate = subtractDays(curr, curr.getDay()); // First date of the week
+    dates.push(firstDate.toLocaleDateString('en-GB').replaceAll('/', '.'))
     optionalDates[dates[0]] = 0
-    for (let i = 1; i < 5; i++) {
-        let nextDay = first + i; // last day is the first day + 4
-        let lastday = new Date(curr.setDate(nextDay)).toLocaleDateString('en-GB').replaceAll('/', '.');
-        optionalDates[lastday] = i
-        dates.push(lastday)
+    for (let i = 0; i < 4; i++) {
+        let nextDate = plusDay(firstDate, i + 1); // 
+        let lastDate = new Date(nextDate).toLocaleDateString('en-GB').replaceAll('/', '.');
+        optionalDates[lastDate] = i + 1
+        dates.push(lastDate)
     }
    
+
+    // Old code:
+    // let curr = new Date(UserFirstDate); // get current date
+    // console.log(UserFirstDate)
+    // let first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
+    // console.log(curr.getDate())
+    // console.log(curr.getDay())
+    // console.log(first)
+    // dates.push(new Date(curr.setDate(first)).toLocaleDateString('en-GB').replaceAll('/', '.'))
+    // optionalDates[dates[0]] = 0
+    // for (let i = 1; i < 5; i++) {
+    //     let nextDay = first + i; // last day is the first day + 4
+    //     let lastday = new Date(curr.setDate(nextDay)).toLocaleDateString('en-GB').replaceAll('/', '.');
+    //     optionalDates[lastday] = i
+    //     dates.push(lastday)
+    // }
+
+
+
     const LecTimeTable = await ConnectionStudLec.find({lecID:LecID})
         .populate('connLessons studID')
         .select("-__v -lecID -connBooks");
